a5e89bfeb40b4ce4e0405918865d5eed9a268876
TasmiyaKhan8028 <155808841+TasmiyaKhan8028@users.noreply.github.com>
2024-05-15 13:24:41 -0700

    add file

---
+++ app.js
@@ -0,0 +1,45 @@
+let img1 = document.getElementById("btn1")
+
+
+let img2 = document.getElementById("btn2")
+img2.addEventListener('mouseover', function() {
+    img1.src = './img/desktop-wallpaper-anime-1280-x-1024-1280x1024-anime-boy-hoodie-blue-eyes-headphones-anime-crash-3d-30…-1280x1024-anime.jpg'
+    
+})
+
+img2.addEventListener('mouseout', function() {
+    img1.src = './img/desktop-wallpaper-8-sad-anime-3d-anime-boys.jpg'
+    img1.style.opacity = "5";
+})
+let img3 = document.getElementById("btn3")
+img3.addEventListener('mouseover', function() {
+    img1.src = './img/images (1).jfif'
+})
+
+img3.addEventListener('mouseout', function() {
+    img1.src = './img/desktop-wallpaper-8-sad-anime-3d-anime-boys.jpg'
+})
+let img4 = document.getElementById("btn4")
+img4.addEventListener('mouseover', function() {
+    img1.src = './img/anime-man-looking-distance-sunset-600nw-2204147469.webp'
+})
+
+img4.addEventListener('mouseout', function() {
+    img1.src = './img/desktop-wallpaper-8-sad-anime-3d-anime-boys.jpg'
+})
+let img5 = document.getElementById("btn5")
+img5.addEventListener('mouseover', function() {
+    img1.src = './img/images.jfif'
+})
+
+img5.addEventListener('mouseout', function() {
+    img1.src = './img/desktop-wallpaper-8-sad-anime-3d-anime-boys.jpg'
+})
+let img6 = document.getElementById("btn6")
+img6.addEventListener('mouseover', function() {
+    img1.src = './img/jujutsu-kaisen-anime-boys-anime-satoru-gojo-hd-wallpaper-preview.jpg'
+})
+
+img6.addEventListener('mouseout', function() {
+    img1.src = './img/desktop-wallpaper-8-sad-anime-3d-anime-boys.jpg'
+})
